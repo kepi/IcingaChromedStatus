a4edb68d9f7fa6748fa54133d208ac8577f38a03
Kepi <kepi@igloonet.cz>
2010-06-20 16:29:47 +0200

    New changes

--- icinga.js
+++ icinga.js
@@ -1,36 +1,25 @@
+// some helper constants
+const STATE_OK = 0;
+const STATE_PEND = 1;
+const STATE_UNKN = 2;
+const STATE_WARN = 4;
+const STATE_CRIT = 8;
+
 function Hosts()
 {
   this.hosts = [];
 
-  this.ok = 0;
-  this.warned = 0;
-  this.critical = 0;
-  this.unknown = 0;
-  this.pending = 0;
-  this.acked = 0;
-
   this.addHost = function(host)
   {
-    this.updateHostState(host);
     this.hosts[host.name] = host;
-  }
-
-  this.updateHostState = function(host)
-  {
-    if ( host.state == 'WARNING' )
-      ++this.warned;
-    else if ( host.state == 'CRITICAL' )
-      ++this.critical;
-    else if ( host.state == 'UNKNOWN' )
-      ++this.unknown;
-    else if ( host.state == 'PENDING' )
-      ++this.pending;
-    else if ( host.state == 'OK' )
-      ++this.ok;
   }
 
   this.setBadge = function()
   {
+    for (var h in this.hosts) {
+
+    }
+    /*
     if ( this.critical > 0 ) {
       color = [255, 255, 0, 255];
       text = this.critical.toString();
@@ -44,8 +33,8 @@ function Hosts()
 
     chrome.browserAction.setBadgeText({text: text});
     chrome.browserAction.setBadgeBackgroundColor({color: color});
+    */
   }
-
 
   this.getHost = function(name)
   {
@@ -55,14 +44,14 @@ function Hosts()
   this.toJSON = function(options)
   {
     var hosts = [];
-    for (s in this.hosts) {
+    for (var h in this.hosts) {
       pushit = true;
 
       if ( options.warned == true )
-        pushit = this.hosts[s].warned ? true : false;
+        pushit = this.hosts[h].getState() ? false : true;
 
       if ( pushit )
-        hosts.push(this.hosts[s].toJSON(options));
+        hosts.push(this.hosts[h].toJSON(options));
     }
 
     return {hosts: hosts};
@@ -74,44 +63,11 @@ function Host(name, link)
   this.name = name;
   this.link = link;
   this.services = new Array;
-  this.state = 'OK';
-
-  this.ok = 0;
-  this.warned = 0;
-  this.critical = 0;
-  this.unknown = 0;
-  this.pending = 0;
-  this.acked = 0;
-
-  this.stateTotal = function()
-  {
-    if ( this.state != 'OK' )
-      return this.state;
-    else if ( this.critical > 0 )
-      return 'CRITICAL';
-    else if ( this.warned > 0 )
-      return 'WARNED';
-    else if ( this.unknown > 0 )
-      return 'UNKNOWN';
-    else if ( this.pending > 0 )
-      return 'PENDING';
-    else
-      return 'OK';
-  }
+  this.state = STATE_OK;
+  this.service_states = {WRONG: 0, WORST: 0, 0: 0, 1: 0, 2: 0, 4: 0, 8: 0};
 
   this.addService = function(service) 
   {
-    if ( service.state == 'WARNING' )
-      ++this.warned;
-    else if ( service.state == 'CRITICAL' )
-      ++this.critical;
-    else if ( service.state == 'UNKNOWN' )
-      ++this.unknown;
-    else if ( service.state == 'PENDING' )
-      ++this.pending;
-    else if ( service.state == 'OK' )
-      ++this.ok;
-    
     this.services[service.name] = service;
   }
 
@@ -120,24 +76,69 @@ function Host(name, link)
     return this.services[name];
   }
 
+  this.getState = function(what)
+  {
+    // only host state, we can return
+    if ( what == 'own' )
+      return this.state;
+
+    // TODO: cachovat
+
+    // we have to check for state
+    for (var s in this.services) {
+      // count this state
+      ++this.service_states[this.services[s].state];
+      // count all wrong states
+      if ( this.services[s].state > STATE_OK )
+        ++this.service_states.WRONG;
+      // set worst state
+      if ( this.services[s].state > this.service_states.WORST )
+        this.service_states.WORST = this.services[s].state;
+    }
+
+    // default - return false if anything is wrong
+    if ( what == undefined ) {
+      return ( this.state == STATE_OK && this.service_states.WRONG == 0 )  ? true : false;
+    } else if ( what == 'worst' ) {
+      return this.service_states.WORST;
+    } else if ( what == 'states_array' ) {
+      return this.service_states;
+    }
+
+  }
+
   this.toJSON = function(options)
   {
     var services = [];
-    for (s in this.services)
+    for (var s in this.services)
       services.push(this.services[s].toJSON(options));
 
-    return {name: this.name, link: this.link, services: services};
+    var states = this.getState('states_array');
+    console.log(states);
+    
+    return {name: this.name, link: this.link, services: services, states: states};
   }
 }
 
 function Service(name, link, state) 
 {
+
+  if ( state == 'OK' )
+    this.state = STATE_OK;
+  else if ( state == 'CRITICAL' )
+    this.state = STATE_CRIT;
+  else if ( state == 'WARNING' )
+    this.state = STATE_WARN;
+  else if ( state == 'UNKNOWN' )
+    this.state = STATE_UNKN;
+  else if ( state == 'PENDING' )
+    this.state = STATE_PEND;
+
   this.name = name;
   this.link = link;
-  this.state = state;
 
   this.toJSON = function(options)
   {
-    return {name: this.name, link: this.link, state: this.state};
+    return {name: this.name, link: this.link, state: this.state.toString()};
   }
 }
